64617fb4fdfb6f79f2eb908ac6c861fd5eaee283
666eronimo <gnimo1705@gmail.com>
2025-11-24 16:13:34 +0700

    grid homapage

--- Component/Grid.tsx
+++ Component/Grid.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 const Grid = () => {
+
+  
+const daysData = [
+  "done","missed","done","done","done","done","done",
+  "done","done","done","done","today","missed","missed",
+  "missed","done","done","done","missed","missed","missed",
+  "missed","missed","missed","missed","missed"
+];
   return (
     <View style={styles.GridContainer}>
       <View style={styles.HeaderGrid}>
@@ -9,7 +17,19 @@ const Grid = () => {
         <Text>2025</Text>
       </View>
 
-      <View style={styles.grid}></View>
+      <View style={styles.grid}>
+        {daysData.map((status, index) => (
+          <View
+            key={index}
+            style={[
+              styles.dayBox,
+              status === "done" && { backgroundColor: "#00A991" },
+              status === "missed" && { backgroundColor: "#B0BEC5" },
+              status === "today" && { backgroundColor: "#FFEB3B" }
+            ]}
+          />
+        ))}
+      </View>
 
       <View style={styles.GrindInfo}>
         <View style={styles.statusItem}>
@@ -45,11 +65,13 @@ const styles = StyleSheet.create({
   },
 
   grid: {
-    height: 214,
-    backgroundColor: "#00A991",
-    borderColor: "#00A991",
     width: "100%",
-    borderWidth: 0,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    rowGap: 14,
+    columnGap: 14,
+    justifyContent: "flex-start",
+    paddingVertical: 10,
   },
 
   statusItem: {
@@ -66,6 +88,11 @@ const styles = StyleSheet.create({
   GridContainer: {
     width: "100%",
     alignSelf: "stretch",
+  },
+   dayBox: {
+    width: 35,
+    height: 35,
+    borderRadius: 6,
   },
 });
 
